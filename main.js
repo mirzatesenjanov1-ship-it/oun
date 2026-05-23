86db4c8f409c5d16e7d7928aa987b57113fa71e3
Сайт 1 <mirzatesenjanov1@gmail.com>
2026-05-23 20:18:20 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+// main.js - Оюнду баштоочу файл
+
+class KumurskaChep {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        const gameConfig = {
+            type: Phaser.AUTO,
+            width: CONFIG.width,
+            height: CONFIG.height,
+            parent: 'game-container',
+            backgroundColor: CONFIG.backgroundColor,
+            physics: {
+                default: 'arcade',
+                arcade: { debug: CONFIG.debug }
+            },
+            scene: [BootScene, PreloadScene, MenuScene, GameScene],
+            scale: {
+                mode: Phaser.Scale.FIT,
+                autoCenter: Phaser.Scale.CENTER_BOTH
+            }
+        };
+
+        this.game = new Phaser.Game(gameConfig);
+        console.log("🚀 Kumurska Chep оюну иштели жатат...");
+    }
+}
+
+// Оюнду жүктөө
+window.onload = () => {
+    new KumurskaChep();
+};
